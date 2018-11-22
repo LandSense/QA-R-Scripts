04a0a8d6c9d209e025c56ecff6cfde8d47f25ba7
Julian Rosser <maptopixel@users.noreply.github.com>
2018-11-22 15:14:57 +0000

    fixed LandUse_qa_v1.R to source from anywhereh

--- LandUse_qa_v1.R
+++ LandUse_qa_v1.R
@@ -4,15 +4,45 @@
 # 05.11.2018, michael.schultz@uni-heidelberg.de
 
 
+LocationOfThisScript = function() # Function LocationOfThisScript returns the location of this .R script (may be needed to source other files in same dir)
+{
+  this.file = NULL
+  # This file may be 'sourced'
+  for (i in -(1:sys.nframe())) {
+    if (identical(sys.function(i), base::source)) this.file = (normalizePath(sys.frame(i)$ofile))
+  }
+  
+  if (!is.null(this.file)) return(dirname(this.file))
+  
+  # But it may also be called from the command line
+  cmd.args = commandArgs(trailingOnly = FALSE)
+  cmd.args.trailing = commandArgs(trailingOnly = TRUE)
+  cmd.args = cmd.args[seq.int(from=1, length.out=length(cmd.args) - length(cmd.args.trailing))]
+  res = gsub("^(?:--file=(.*)|.*)$", "\\1", cmd.args)
+  
+  # If multiple --file arguments are given, R uses the last one
+  res = tail(res[res != ""], 1)
+  if (0 < length(res)) return(dirname(res))
+  
+  # Both are not the case. Maybe we are in an R GUI?
+  return(NULL)
+}
+
+
+
+
 LandUse_qa_v1=function(In, Out, a, n, s){ # arguments: In = Input data, Out = Output data, a = campaign prefix, n = redundancy depth if 0 or 1 than no redundancy (needs implementation), s = sampling population 
   
   # dependencies
   x <- c('jsonlite', 'rgdal', 'reshape2', 'ggplot2', 'raster') # list of libraries
   lapply(x, library, character.only = T); rm(x) # load libraries and clean
-  fd <- '/home/michi/Dropbox/LandSense_QA/' # functions home
-  source(paste0(fd, 'ScottsPi_v1.R')) # Scotts Pi, https://en.wikipedia.org/wiki/Fleiss%27_kappa # worked example
-  source(paste0(fd, 'fun_accuracy_stratified.R')) # accuracy Card 1982
+  fd <- LocationOfThisScript()
   
+  
+  print(paste0('working dir ', fd))
+  source(paste(fd, 'ScottsPi_v1.R', sep="/")) # Scotts Pi, https://en.wikipedia.org/wiki/Fleiss%27_kappa # worked example
+  source(paste(fd, 'fun_accuracy_stratified.R', sep="/")) # accuracy Card 1982
+
   # /home/michi/Dropbox/LandSense_QA/LandUse_qa/in/ /home/michi/Dropbox/LandSense_QA/LandUse_qa/out/ GENEVA 5 150
   
   #In <- '/home/michi/Dropbox/LandSense_QA/LandUse_qa/in/'
@@ -21,9 +51,17 @@ LandUse_qa_v1=function(In, Out, a, n, s){ # arguments: In = Input data, Out = Ou
   #n <- 5
   #s <- 150
   
+  # C:/Users/ezzjfr/Documents/lucas_git/landsenseR/LandSense_QA/LandUse_qa/In/ C:/Users/ezzjfr/Documents/lucas_git/landsenseR/LandSense_QA/LandUse_qa/Out/ GENEVAJ 5 150
+  
+  # In <-'C:/Users/ezzjfr/Documents/lucas_git/landsenseR/LandSense_QA/LandUse_qa/In/'
+  # Out <- 'C:/Users/ezzjfr/Documents/lucas_git/landsenseR/LandSense_QA/LandUse_qa/Out/'
+  # a <- 'GENEVAJules'
+  # n <- 5
+  # s <- 150
+  
   # program start
   setwd(In) # Input data 
-
+  
   fl <- list.files(pattern = paste0('*', '.geojson')) # geojson list
   r <- raster(list.files(pattern = paste0('*', '.tif'))) # tif raster file
   
@@ -85,7 +123,7 @@ LandUse_qa_v1=function(In, Out, a, n, s){ # arguments: In = Input data, Out = Ou
   d$con <- round(d$con, digits = 2) # additional round
   
   # create accuracy comparison figure
- ggplot(d, aes(x = lc, y = acc, fill = type)) + # call plot
+  ggplot(d, aes(x = lc, y = acc, fill = type)) + # call plot
     geom_bar(position=position_dodge(), stat="identity")+ # basic bar plot
     geom_errorbar(aes(ymin=acc-con, ymax=acc+con), position=position_dodge()) + # errorbars
     geom_hline(yintercept = 85, color="green", linetype="dashed", size = 1)+ 
@@ -100,8 +138,8 @@ LandUse_qa_v1=function(In, Out, a, n, s){ # arguments: In = Input data, Out = Ou
           axis.ticks = element_line(colour = 'black', size = 1), legend.position="bottom", 
           strip.background = element_blank(),
           strip.text = element_text(size = 15)) # axis ticks
-ggsave(paste0(Out, a, '_Accuracy', '.jpg'), width = 4, height = 4)
-
+  ggsave(paste0(Out, a, '_Accuracy', '.jpg'), width = 4, height = 4)
+  
 }
 
 args <- commandArgs(T) # parse arguments
